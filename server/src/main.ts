aac9f54d8ecc467bd40a448cf2cc276b6ee835f2
Shreedhar Hegde <snhkelemane@gmail.com>
2024-09-27 22:00:00 +0530

    added request logger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableShutdownHooks();
+  app.use(morgan('dev'));
   app.useGlobalPipes(new ValidationPipe());
+  app.enableShutdownHooks();
   await app.listen(3000);
 }
 bootstrap();
