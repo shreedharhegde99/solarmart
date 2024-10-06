79e8281d1e8076f988fe78590055e76dc13c9894
Shreedhar Hegde <snhkelemane@gmail.com>
2024-10-06 12:27:22 +0530

    added component lazy loading

--- ui/src/components/routes/UserRoutes.tsx
+++ ui/src/components/routes/UserRoutes.tsx
@@ -1,16 +1,24 @@
 import { Route, Routes } from "react-router-dom";
-import Signup from "../auth/Signup";
-import UserAuth from "../auth/UserAuth";
-import Home from "../Home";
-import AllProducts from "../products/AllProducts";
+import HomePageComponent from "../home-page";
+import { lazy } from "react";
+
+const ProductsListingComponent = lazy(() => import("../products-listing"));
+const ProductPageComponent = lazy(() => import("../product-page"));
+const AboutUsPageComponent = lazy(() => import("../product-page"));
+const SignupPage = lazy(() => import("../signup-page"));
+const LoginPageComponent = lazy(() => import("../login-page"));
+const CheckoutPageComponent = lazy(() => import("../checkout-page"));
 
 export default function UserRoutes() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<UserAuth />} />
-      <Route path="/products" element={<AllProducts />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<HomePageComponent />} />
+			<Route path="/about-us" element={<AboutUsPageComponent />} />
+			<Route path="/signup" element={<SignupPage />} />
+			<Route path="/login" element={<LoginPageComponent />} />
+			<Route path="/products" element={<ProductsListingComponent />} />
+			<Route path="/products/:product-name" element={<ProductPageComponent />} />
+			<Route path="/cart" element={<CheckoutPageComponent />} />
+		</Routes>
+	);
 }
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from "react";
+import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import LoadingPage from "./components/loading-page.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+	<StrictMode>
+		<BrowserRouter>
+			<Suspense fallback={<LoadingPage />}>
+				<App />
+			</Suspense>
+		</BrowserRouter>
+	</StrictMode>,
 );
